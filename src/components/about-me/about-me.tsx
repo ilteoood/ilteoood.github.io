0eb7fcc43360a9c52bdab71f90710c7ee0421d56
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-12-15 10:57:50 +0100

    About me

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -7,10 +7,10 @@ export const AboutMe: React.FC = () => {
             <img className="w-32 h-auto rounded-full" src={me} alt="Me"/>
             <div className="p-8 text-left my-auto">
                 <figcaption className="font-medium">
-                    <div className="text-cyan-600">
+                    <div className="text-color">
                         Matteo Pietro Dazzi
                     </div>
-                    <a className="text-gray-500" href="https://telegram.me/iLTeoooD">
+                    <a href="https://telegram.me/iLTeoooD">
                         @iLTeoooD
                     </a>
                 </figcaption>
