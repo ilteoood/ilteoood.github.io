651aded4c1b8f8c530db1afb5a1d4e03a96caa77
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-12-16 14:16:39 +0100

    Improvements

--- src/sections/introduction/introduction.tsx
+++ src/sections/introduction/introduction.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import me from '../../resources/me.png';
 
 export const Introduction: React.FC = () => {
     return (
         <figure className="flex place-content-center items-center pt-8 md:pt-16">
-            <img className="ml-4 md:ml-0 w-32 h-32 rounded-full" src={me} alt="Me"/>
+            <img className="ml-4 md:ml-0 w-32 h-32 rounded-full"
+                 src="https://avatars2.githubusercontent.com/u/6383527?s=460&u=9cec6af101e42574a87753cf26788425d11db5cb&v=4"
+                 alt="Me"/>
             <div className="md:p-4 xl:p-8 pl-4 text-left">
                 <blockquote className="text-lg text-color italic">
                     "I'm looking for something that never makes me stop keeping up with the future."
