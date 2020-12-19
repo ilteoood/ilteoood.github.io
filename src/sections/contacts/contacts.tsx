d7913faae56ecdad38456617e01156efbd08e9fa
iLTeoooD <matteopietro.dazzi@gmail.com>
2020-12-19 12:48:06 +0100

    Contacts icons colors

--- src/sections/contacts/contacts.tsx
+++ src/sections/contacts/contacts.tsx
@@ -43,7 +43,22 @@ export const Contacts: React.FC = () => {
     return (
         <div className="contacts-container" id="contacts">
             {contacts.map(iconMapper)}
+            <InstagramGradient/>
         </div>
     );
 
+}
+
+const InstagramGradient: React.FC = () => {
+    return (
+        <svg width="0" height="0">
+            <radialGradient id="instagradient" r="150%" cx="30%" cy="107%">
+                <stop stop-color="#fdf497" offset="0"/>
+                <stop stop-color="#fdf497" offset="0.05"/>
+                <stop stop-color="#fd5949" offset="0.45"/>
+                <stop stop-color="#d6249f" offset="0.6"/>
+                <stop stop-color="#285AEB" offset="0.9"/>
+            </radialGradient>
+        </svg>
+    );
 }
