67d7b7538346613bcebf9b2fb50a0fa26434ad15
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2026-05-06 15:57:00 +0200

    fix: format

--- src/types/repository.ts
+++ src/types/repository.ts
@@ -1,13 +1,13 @@
 export interface Repository {
-  id: number;
-  name: string;
-  description: string | null;
-  html_url: string;
-  stargazers_count: number;
-  forks_count: number;
-  watchers_count: number;
-  open_issues_count: number;
-  updated_at: string;
-  fork: boolean;
-  archived: boolean;
+  id: number
+  name: string
+  description: string | null
+  html_url: string
+  stargazers_count: number
+  forks_count: number
+  watchers_count: number
+  open_issues_count: number
+  updated_at: string
+  fork: boolean
+  archived: boolean
 }
